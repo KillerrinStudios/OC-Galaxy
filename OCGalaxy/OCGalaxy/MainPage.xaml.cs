fa2598c58e9b708cd1e71cfeb6dfb63b08f85cc2
Andrew Godfroy <andrew.godfroy@killerrin.com>
2020-03-08 22:26:43 -0400

    Initial Commit

---
+++ OCGalaxy/OCGalaxy/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tizen.Wearable.CircularUI.Forms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OCGalaxy
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : CirclePage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
