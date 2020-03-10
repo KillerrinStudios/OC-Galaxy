24373c617f78a9c8d2cc04980697e583c86237ac
Andrew Godfroy <andrew.godfroy@killerrin.com>
2020-03-10 01:25:20 -0400

    Added OC Search State for Widget UI State Management

---
+++ OCGalaxy/OCGalaxy/Models/OCSearchState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCGalaxy.Models
+{
+    public enum OCSearchState
+    {
+        Stopped,
+        Searching
+    }
+}
