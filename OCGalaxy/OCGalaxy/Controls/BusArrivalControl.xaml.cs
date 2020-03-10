751974eea260ddc685e2b1401211205556dd554d
Andrew Godfroy <andrew.godfroy@killerrin.com>
2020-03-10 01:24:51 -0400

    Began implementing Mockups as reusable Control

---
+++ OCGalaxy/OCGalaxy/Controls/BusArrivalControl.xaml.cs
@@ -0,0 +1,114 @@
+﻿using OCGalaxy.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OCGalaxy.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BusArrivalControl : ContentView
+    {
+        public static readonly BindableProperty RouteNumberProperty = BindableProperty.Create(
+            propertyName: nameof(RouteNumber),
+            returnType: typeof(string),
+            declaringType: typeof(BusArrivalControl),
+            defaultValue: "",
+            defaultBindingMode: Xamarin.Forms.BindingMode.OneWay,
+            propertyChanged: RouteNumberPropertyChanged);
+        public string RouteNumber
+        {
+            get { return (string)GetValue(RouteNumberProperty); }
+            set { SetValue(RouteNumberProperty, value); }
+        }
+
+        public static readonly BindableProperty RouteNameProperty = BindableProperty.Create(
+            propertyName: nameof(RouteName),
+            returnType: typeof(string),
+            declaringType: typeof(BusArrivalControl),
+            defaultValue: "",
+            defaultBindingMode: Xamarin.Forms.BindingMode.OneWay,
+            propertyChanged: RouteNamePropertyChanged);
+        public string RouteName
+        {
+            get { return (string)GetValue(RouteNameProperty); }
+            set { SetValue(RouteNameProperty, value); }
+        }
+
+        public static readonly BindableProperty ArrivalTimeProperty = BindableProperty.Create(
+            propertyName: nameof(ArrivalTime),
+            returnType: typeof(string),
+            declaringType: typeof(BusArrivalControl),
+            defaultValue: "",
+            defaultBindingMode: Xamarin.Forms.BindingMode.OneWay,
+            propertyChanged: ArrivalTimePropertyChanged);
+        public string ArrivalTime
+        {
+            get { return (string)GetValue(ArrivalTimeProperty); }
+            set { SetValue(ArrivalTimeProperty, value); }
+        }
+
+        public static readonly BindableProperty StopNameProperty = BindableProperty.Create(
+            propertyName: nameof(StopName),
+            returnType: typeof(string),
+            declaringType: typeof(BusArrivalControl),
+            defaultValue: "",
+            defaultBindingMode: Xamarin.Forms.BindingMode.OneWay,
+            propertyChanged: StopNamePropertyChanged);
+        public string StopName
+        {
+            get { return (string)GetValue(StopNameProperty); }
+            set { SetValue(StopNameProperty, value); }
+        }
+
+        public static readonly BindableProperty IsGPSProperty = BindableProperty.Create(
+            propertyName: nameof(IsGPS),
+            returnType: typeof(bool),
+            declaringType: typeof(BusArrivalControl),
+            defaultValue: false,
+            defaultBindingMode: Xamarin.Forms.BindingMode.OneWay,
+            propertyChanged: IsGPSPropertyChanged);
+        public bool IsGPS
+        {
+            get { return (bool)GetValue(IsGPSProperty); }
+            set { SetValue(IsGPSProperty, value); }
+        }
+
+        public BusArrivalControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void RouteNumberPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (BusArrivalControl)bindable;
+            control.RouteNumberLabel.Text = newValue.ToString();
+        }
+        private static void RouteNamePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (BusArrivalControl)bindable;
+            control.RouteNameLabel.Text = newValue.ToString();
+        }
+        private static void ArrivalTimePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (BusArrivalControl)bindable;
+            control.ArrivalTimeLabel.Text = newValue.ToString();
+        }
+        private static void StopNamePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (BusArrivalControl)bindable;
+            control.StopNameLabel.Text = newValue.ToString();
+        }
+        private static void IsGPSPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (BusArrivalControl)bindable;
+            control.GPSIcon.IsVisible = (bool)newValue;
+        }
+
+    }
+}
