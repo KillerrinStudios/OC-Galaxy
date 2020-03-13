7bc3877e7cb3f04f093e98fb5af11b5e01fb28f8
Andrew Godfroy <andrew.godfroy@killerrin.com>
2020-03-13 02:20:05 -0400

    Added additional NuGet packages

--- OCGalaxy/OCGalaxy.Tizen.Widget.Wearable/OCGalaxy.Tizen.Widget.Wearable.cs
+++ OCGalaxy/OCGalaxy.Tizen.Widget.Wearable/OCGalaxy.Tizen.Widget.Wearable.cs
@@ -1,4 +1,7 @@
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Diagnostics;
+using System.IO;
 using Tizen.Applications;
 using Tizen.Wearable.CircularUI.Forms.Renderer;
 using Tizen.Wearable.CircularUI.Forms.Renderer.Widget;
@@ -11,6 +14,10 @@ namespace OCGalaxy.Tizen.Wearable.Widget
         public override void OnCreate(Bundle content, int w, int h)
         {
             base.OnCreate(content, w, h);
+
+            //var directory = "apps_rw/org.tizen.KillerrinStudios.OCGalaxy.Tizen.Widget.Wearable";
+            //var secrets = "ec399b9d-08bf-4dd8-8d30-31c3bdeeea61";
+
             var app = new App(Models.OCApplicationType.Widget);
             LoadApplication(app);
         }
