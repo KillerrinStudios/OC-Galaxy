154b7d4b705550fe0b74fd9e3454fed642b4d55e
Andrew Godfroy <andrew.godfroy@killerrin.com>
2020-03-13 02:20:43 -0400

    Added MenuItem Models for Menu Modelbinding purposes

---
+++ OCGalaxy/OCGalaxy/Models/MenuItemModel.cs
@@ -0,0 +1,85 @@
+﻿using Killerrin.Toolkit.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace OCGalaxy.Models
+{
+    public class MenuItemModel : ModelBase
+    {
+        private string _text = "";
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value;
+                RaisePropertyChanged(nameof(Text));
+            }
+        }
+
+        private string _subtext = "";
+        public string SubText
+        {
+            get => _subtext;
+            set
+            {
+                _subtext = value;
+                RaisePropertyChanged(nameof(SubText));
+            }
+        }
+
+        private string _iconImageSource = "";
+        public string IconImageSource
+        {
+            get => _iconImageSource;
+            set
+            {
+                _iconImageSource = value;
+                RaisePropertyChanged(nameof(IconImageSource));
+            }
+        }
+
+        private ICommand _action = null;
+        public ICommand Action
+        {
+            get => _action;
+            set
+            {
+                _action = value;
+                RaisePropertyChanged(nameof(Action));
+            }
+        }
+
+        private bool _enabled = true;
+        public bool Enabled
+        {
+            get => _enabled;
+            set
+            {
+                _enabled = value;
+                RaisePropertyChanged(nameof(Enabled));
+            }
+        }
+
+        private bool _visible = true;
+        public bool Visible
+        {
+            get => _visible;
+            set
+            {
+                _visible = value;
+                RaisePropertyChanged(nameof(Visible));
+            }
+        }
+
+        public MenuItemModel(string text, string subtext, string iconImageSource, ICommand action)
+        {
+            Text = text;
+            SubText = subtext;
+            IconImageSource = iconImageSource;
+            Action = action;
+        }
+    }
+}
---
+++ OCGalaxy/OCGalaxy/Models/ToggleMenuItemModel.cs
@@ -0,0 +1,72 @@
+﻿using Killerrin.Toolkit.Core.Models;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace OCGalaxy.Models
+{
+    public class ToggleMenuItemModel : ModelBase
+    {
+        private MenuItemModel _onState = null;
+        public MenuItemModel OnState
+        {
+            get => _onState;
+            protected set
+            {
+                _onState = value;
+                RaisePropertyChanged(nameof(OnState));
+
+            }
+        }
+
+        private MenuItemModel _offState = null;
+        public MenuItemModel OffState
+        {
+            get => _offState;
+            protected set
+            {
+                _offState = value;
+                RaisePropertyChanged(nameof(OffState));
+            }
+        }
+
+        private MenuItemModel _currentState = null;
+        public MenuItemModel CurrentState
+        {
+            get => _currentState;
+            private set
+            {
+                _currentState = value;
+                RaisePropertyChanged(nameof(CurrentState));
+            }
+        }
+
+        public bool ToggleState { get; private set; } = false;
+        public ICommand Toggle { get { return new Command(() => { ToggleCurrentState(); }); } }
+        public ICommand Reset { get { return new Command(() => { SetCurrentState(); }); } }
+
+        public ToggleMenuItemModel(bool currentState, MenuItemModel offState, MenuItemModel onState)
+        {
+            ToggleState = currentState;
+            OffState = offState;
+            OnState = onState;
+
+            SetCurrentState();
+        }
+
+        public void ToggleCurrentState()
+        {
+            RaisePropertyChanging(nameof(ToggleState));
+            ToggleState = !ToggleState;
+            SetCurrentState();
+            RaisePropertyChanged(nameof(ToggleState));
+
+            CurrentState.Action?.Execute(null);
+        }
+
+        public void SetCurrentState()
+        {
+            if (ToggleState) { CurrentState = OnState; }
+            else { CurrentState = OffState; }
+        }
+    }
+}
