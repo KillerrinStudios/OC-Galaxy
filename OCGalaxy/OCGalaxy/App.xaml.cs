5abb49965bb0405760dd653ad08be8eb2536c61a
Andrew Godfroy <andrew.godfroy@killerrin.com>
2020-03-13 02:22:05 -0400

    Cleaned up code in App.xaml.cs

--- OCGalaxy/OCGalaxy/App.xaml.cs
+++ OCGalaxy/OCGalaxy/App.xaml.cs
@@ -2,8 +2,10 @@
 using OCGalaxy.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 using Xamarin.Forms;
@@ -15,20 +17,13 @@ namespace OCGalaxy
     public partial class App : Application
     {
         public static App GetCurrentApp() { return (App)App.Current; }
-
         public OCApplicationType ocApplicationType;
-        public IConfiguration Configuration;
 
         public App(OCApplicationType appType)
         {
             ocApplicationType = appType;
             InitializeComponent();
 
-            var builder = new ConfigurationBuilder()
-                //.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddUserSecrets("8aa55baf-e1e1-40c2-a1bb-669855870a4a");
-            Configuration = builder.Build();
-           
             if (appType == OCApplicationType.Application)
             {
                 MainPage = new MainPage();
